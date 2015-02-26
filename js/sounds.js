7f2b1cf082f7fbd5ab3f31d85aefbc25e60a9dbf
elainelylee <elaine@generalassemb.ly>
2015-02-25 22:24:40 -0500

    cool

---
+++ js/sounds.js
@@ -0,0 +1,91 @@
+// $(document).ready(function(){
+//   $(".pad-1")
+//       .on('mousedown', function(e){
+//           var clicked=$(e.currentTarget);
+//           clicked.find("img").attr("src","img/circle.png")
+//           clicked.find("audio")[0].play();
+//       })
+//       .on('mouseup', function(e){
+//           var clicked=$(e.currentTarget);
+//           clicked.find("img").attr("src","img/square.jpg")
+//       });
+
+//   $(".pad-2")
+//       .on('mousedown', function(e){
+//           var clicked=$(e.currentTarget);
+//           clicked.find("img").attr("src","img/i.png")
+//           clicked.find("audio")[0].play();
+//       })
+//       .on('mouseup', function(e){
+//           var clicked=$(e.currentTarget);
+//           clicked.find("img").attr("src","img/square.jpg")
+//       });
+
+//    $(".pad-3")
+//       .on('mousedown', function(e){
+//           var clicked=$(e.currentTarget);
+//           clicked.find("img").attr("src","img/line.png")
+//           clicked.find("audio")[0].play();
+//       })
+//       .on('mouseup', function(e){
+//           var clicked=$(e.currentTarget);
+//           clicked.find("img").attr("src","img/square.jpg")
+//       });
+
+//     $(".pad-4")
+//       .on('mousedown', function(e){
+//           var clicked=$(e.currentTarget);
+//           clicked.find("img").attr("src","img/diamond.png")
+//           clicked.find("audio")[0].play();
+//       })
+//       .on('mouseup', function(e){
+//           var clicked=$(e.currentTarget);
+//           clicked.find("img").attr("src","img/square.jpg")
+//       });         
+// })
+
+$(document).ready(function(){
+  $(".pad-1")
+      .on('mouseover', function(e){
+          var clicked=$(e.currentTarget);
+          clicked.find("img").attr("src","img/circle.png")
+          clicked.find("audio")[0].play();
+      })
+      .on('mouseout', function(e){
+          var clicked=$(e.currentTarget);
+          clicked.find("img").attr("src","img/square.jpg")
+      });
+
+  $(".pad-2")
+      .on('mouseover', function(e){
+          var clicked=$(e.currentTarget);
+          clicked.find("img").attr("src","img/i.png")
+          clicked.find("audio")[0].play();
+      })
+      .on('mouseout', function(e){
+          var clicked=$(e.currentTarget);
+          clicked.find("img").attr("src","img/square.jpg")
+      });
+
+   $(".pad-3")
+      .on('mouseover', function(e){
+          var clicked=$(e.currentTarget);
+          clicked.find("img").attr("src","img/line.png")
+          clicked.find("audio")[0].play();
+      })
+      .on('mouseout', function(e){
+          var clicked=$(e.currentTarget);
+          clicked.find("img").attr("src","img/square.jpg")
+      });
+
+    $(".pad-4")
+      .on('mouseover', function(e){
+          var clicked=$(e.currentTarget);
+          clicked.find("img").attr("src","img/diamond.png")
+          clicked.find("audio")[0].play();
+      })
+      .on('mouseout', function(e){
+          var clicked=$(e.currentTarget);
+          clicked.find("img").attr("src","img/square.jpg")
+      });         
+})
